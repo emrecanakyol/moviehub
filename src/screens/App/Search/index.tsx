5937d79609389e83c7e6b9f320e721f2e4df4e2a
Emre Can Akyol <emrecanakyoll@gmail.com>
2025-02-09 16:11:25 +0300

    feat: When you click on the movie in the search results, you are directed to the detail page.

--- src/screens/App/Search/index.tsx
+++ src/screens/App/Search/index.tsx
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
-import { View, FlatList, Image } from 'react-native';
+import { View, FlatList, Image, TouchableOpacity } from 'react-native';
 import { TextInput, Card, Title, ActivityIndicator } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store/store';
 import { styles } from './styles';
-
+import { useNavigation } from '@react-navigation/native';
+import { MOVIES_DETAILS } from '../../../navigators/Stack';
 
 const SearchScreen = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const { movies, loading } = useSelector((state: RootState) => state.movies);
+    const navigation = useNavigation<any>();
 
     const filteredMovies = movies.filter((movie) =>
         movie.original_title.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
     const renderMovieItem = ({ item }: { item: { movie_id: number; original_title: string; poster_path: string } }) => (
-        <Card style={styles.card}>
-            <View style={styles.cardContent}>
-                <Image
-                    source={{ uri: item.poster_path }}
-                    style={styles.poster}
-                />
-                <Card.Content>
-                    <Title style={styles.movieTitle}>{item.original_title}</Title>
-                </Card.Content>
-            </View>
-        </Card>
+        <TouchableOpacity
+            onPress={() => {
+                navigation.navigate(MOVIES_DETAILS, { movie: item });
+            }}
+        >
+            <Card style={styles.card}>
+                <View style={styles.cardContent}>
+                    <Image
+                        source={{ uri: item.poster_path }}
+                        style={styles.poster}
+                    />
+                    <Card.Content>
+                        <Title style={styles.movieTitle}>{item.original_title}</Title>
+                    </Card.Content>
+                </View>
+            </Card>
+        </TouchableOpacity>
     );
 
     return (
